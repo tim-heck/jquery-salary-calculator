f02768e0c431ac7765aff48c831250e7524a966f
Tim Heck <tih99924@bethel.edu>
2019-06-21 19:27:45 -0500

    delete employee working with updated monthly cost

--- scripts/client.js
+++ scripts/client.js
@@ -3,15 +3,6 @@ $(document).ready(onReady);
 // List of employees
 let employeeList = [];
 
-let firstEmployee = {
-    firstName: 'Tim',
-    lastName: 'Heck',
-    id: 1234,
-    title: 'Master of Illusion',
-    annualSalary: 0.01
-}
-employeeList.push(firstEmployee);
-
 /**
  * Function that will collect all employee's information from inputs,
  * create an newEmplyoee object and store that information there.
@@ -21,15 +12,14 @@ employeeList.push(firstEmployee);
 function storeEmployee(event) {
     event.preventDefault();
     let newEmployee = {
+        annualSalary: $('#annualSalaryIn').val(),
         firstName: $('#firstNameIn').val(),
-        lastName: $('#lastNameIn').val(),
         id: $('#idIn').val(),
+        lastName: $('#lastNameIn').val(),
         title: $('#titleIn').val(),
-        annualSalary: $('#annualSalaryIn').val()
     }
     employeeList.push(newEmployee);
-    $('.employeeInput').val('');
-    console.log(employeeList);
+    // $('.employeeInput').val('');
     showEmployee();
 }
 
@@ -38,19 +28,55 @@ function storeEmployee(event) {
  * Also adds each employees salary to the totalSalary to be displayed
  */
 function showEmployee() {
+    // Total monthly cost of employees
+    let totalMonthyCost = 0;
+    // Total salary of employees
+    let totalAnnualSalary = 0;
+    let el = $('#employeeList');
+    el.empty();
     for (let i = 0; i < employeeList.length; i++) {
-        $('#employeeList').append(`
+        el.append(`
         <tr>
             <th>${employeeList[i].firstName}</th>
             <th>${employeeList[i].lastName}</th>
             <th>${employeeList[i].id}</th>
             <th>${employeeList[i].title}</th>
-            <th>${employeeList[i].annualSalary}</th>
-            <th><button id="deleteEmployee" class="btn btn-danger">Delete</button></th>
+            <th>$${employeeList[i].annualSalary}</th>
+            <th><button class="btn btn-danger delete">Delete</button></th>
         </tr>`);
-
+        totalAnnualSalary += Number(employeeList[i].annualSalary);
     }
+    totalMonthyCost = totalAnnualSalary / 12;
+    $('#totalSalary').html('$' + totalMonthyCost.toFixed(2));
+    if (totalMonthyCost > 20000) {
+        $('#totalSalary').parent().addClass('red-background');
+    }
+}
 
+/**
+ * Function that checks the element's text with the text of the
+ * employee object inside of
+ */
+function deleteEmployee() {
+    let employeeInfo = $(this).parent().siblings().text();
+    console.log(employeeInfo);
+    let employeeInfoCheck = '';
+    const numOfEmployees = employeeList.length;
+    for (let i = 0; i < numOfEmployees; i++) {
+        employeeInfoCheck = employeeList[i].firstName +
+            employeeList[i].lastName +
+            employeeList[i].id +
+            employeeList[i].title +
+            employeeList[i].annualSalary;
+        console.log(employeeInfoCheck);
+        employeeInfo = employeeInfo.replace('$', '');
+        if (employeeInfoCheck === employeeInfo) {
+            employeeList.splice(i, 1);
+            break;
+        }
+    }
+    console.log(employeeList);
+    showEmployee();
 }
 
 /**
@@ -58,5 +84,5 @@ function showEmployee() {
  */
 function onReady() {
     $('#submitEmployee').on('click', storeEmployee);
-    showEmployee();
+    $('#employeeList').on('click', '.delete', deleteEmployee);
 }
